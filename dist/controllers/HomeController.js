a1a551413783e34a46a8aad429b41d14f5d70357
Matheus Muniz <103901768+MunizMat@users.noreply.github.com>
2023-02-22 09:26:05 -0300

    Build

---
+++ dist/controllers/HomeController.js
@@ -0,0 +1,7 @@
+"use strict";Object.defineProperty(exports, "__esModule", {value: true});class HomeController {
+  async store(req, res) {
+    res.send('oi');
+  }
+}
+
+exports. default = new HomeController();
